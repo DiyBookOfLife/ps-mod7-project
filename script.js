330e4d91ade3fe97577f1828e041a38576c63cde
Toni Thomas <tsthomas205@gmail.com>
2026-02-03 12:03:11 -0500

    Updated fetch URL with field queries

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 async function checkAPI() {
   try {
-    const response = await fetch("https://restcountries.com/v3.1/name/all");
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,population,region,capital",
+    );
     if (!response.ok) {
       throw new Error(`Error! Status: ${response.status}`);
     }
@@ -12,3 +14,12 @@ async function checkAPI() {
 }
 
 checkAPI();
+
+// data.forEach((item) => {
+//   function renderCountry(data) {
+//     document.getElementById("name").textContent = data.name.common;
+//     document.getElementById("population").textContent = data.population;
+//     document.getElementById("region").textContent = data.region;
+//     document.getElementById("capital").textContent = data.capital;
+//   }
+//   renderCountry(item);
